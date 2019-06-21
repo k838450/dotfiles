a7729a4caa89d5cde9ff5656d2f52480a02da932
s-now <k838450@gmail.com>
2019-06-21 22:52:23 +0900

    commit

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# check commands
+if !(type "git" > /dev/null 2>&1); then
+	echo "please install git"
+	i = 1
+fi
+
+if !(type "curl" > /dev/null 2&1); then
+	echo "please install curl"
+	i = 1
+fi
+
+if [i = 1]; then
+	exit 0
+fi
+
+git clone https://github.com/k838450/dotfiles.git ./dotfiles
+
+# install deinvim
+echo "start install deinvim"
+mkdir -p ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+sed -i -e '0,10' ./tmp.txt
+sed -i -e '1,10d' test.txt
+sed -i -e '36,39d' test.txt
+cp ./dotfiles/.vimrc  ~/.vimrc
+echo "success"
